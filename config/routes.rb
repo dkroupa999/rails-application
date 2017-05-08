55363a322b4497c0e15a26f906ac4725cbe8846f
Daniel Kroupa <dkroupa@gmail.com>
2017-05-08 20:23:28 +1000

    working login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'static_pages#home'
+  get '/contact-us', to:'static_pages#contact'
+  get '/about-us', to: 'static_pages#about'
+  get '/helloworld', to: 'static_pages#helloworld'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
